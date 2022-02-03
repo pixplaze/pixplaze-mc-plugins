399e9d67dd9b8b687d00f24bec6355c12ac01053
pazik98 <nefedow98@mail.ru>
2022-02-04 02:29:32 +0300

    Исправлена ошибка, возникающая при попытке чтения несуществующего `userprofile.json`
    	-	Часть функционала вынесена из метода `onPlayerJoin()` в отдельный метод `saveProfile()`.
    	-	Реализована проверка на существование `userprofile.json`.

--- src/main/java/com/pixplaze/simpleeventtracker/listener/PlayerListener.java
+++ src/main/java/com/pixplaze/simpleeventtracker/listener/PlayerListener.java
@@ -8,38 +8,52 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.checkerframework.checker.units.qual.A;
 
 import java.io.*;
 import java.lang.reflect.Type;
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Logger;
 
 public class PlayerListener implements Listener {
 
+    private static final Logger logger = SimpleEventTracker.getInstance().getLogger();
     private static final Gson gson = new Gson();
+    private static final String filepath = SimpleEventTracker.getInstance().getDataFolder() + "/userprofile.json";
 
     @EventHandler
     public static void onPlayerJoin(PlayerJoinEvent e) {
-        Player player = e.getPlayer();
-        var profile = getPlayerProfile(player);
-
-        String joinHistoryData = readFromFile(SimpleEventTracker.getInstance().getDataFolder() +
-                "/userprofile.json");
-
-        /* Десериализуем прочитанный файл и добавляем к нему новую запись */
-        Type playerProfileList = new TypeToken<ArrayList<PlayerProfile>>(){}.getType();
-        List<PlayerProfile> joinHistory = gson.fromJson(joinHistoryData, playerProfileList);
-        joinHistory.add(profile);
-        writeToFile(gson.toJson(joinHistory));
+        var profile = getPlayerProfile(e.getPlayer());
+        saveProfile(profile);
     }
 
-    static private PlayerProfile getPlayerProfile(Player player) {
+    private static PlayerProfile getPlayerProfile(Player player) {
         return new PlayerProfile(player.getUniqueId(), player.getName());
     }
 
-    private static void writeToFile(String json) {
-        var file = new File(SimpleEventTracker.getInstance().getDataFolder() + "/userprofile.json");
+    private static void saveProfile(PlayerProfile profile) {
+        var file = new File(filepath);
+        List<PlayerProfile> joinHistory = new ArrayList<>();
+
+        try {
+            if (file.createNewFile()) {
+                logger.info("userprofile.json doesn't exist. Creating...");
+            } else {
+                var joinHistoryData = readFromFile(file);
+                /* Десериализуем прочитанный файл */
+                Type playerProfileList = new TypeToken<ArrayList<PlayerProfile>>(){}.getType();
+                joinHistory = gson.fromJson(joinHistoryData, playerProfileList);
+            }
+            joinHistory.add(profile);
+            writeToFile(file, gson.toJson(joinHistory));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void writeToFile(File file, String json) {
         try (FileWriter fw = new FileWriter(file)) {
             fw.write(json);
         } catch (IOException e) {
@@ -47,11 +61,10 @@ public class PlayerListener implements Listener {
         }
     }
 
-    private static String readFromFile(String path) {
+    private static String readFromFile(File file) {
         String data = "";
 
-        try (var br = new BufferedReader(new InputStreamReader(new FileInputStream(path),
-                StandardCharsets.UTF_8))) {
+        try (var br = new BufferedReader(new FileReader(file))) {
             String line;
             while ((line = br.readLine()) != null) {
                 data += line;
